21c9aec83f730fc8a428d9f8df365e5522ba029e
Jakob <jakobdellago@gmail.com>
2018-05-27 20:17:54 +0200

    task: -mask setup milestone -> first mask-element gets rendered

---
+++ fileadmin/resources/typoscript/mask.ts
@@ -0,0 +1,5 @@
+page < temp.mask.page
+page.10.file.stdWrap.cObject.default.value = fileadmin\resources\templates\main.html
+
+page.10.variables.content < styles.content.get
+page.10.variables.content.select.where = colPos=10
