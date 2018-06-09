30ef1719e89a07468f582bd1c5c3fdc637e96bd5
Jakob <jakobdellago@gmail.com>
2018-06-09 19:15:57 +0200

    clean up
    clean up html and typoscript

--- fileadmin/resources/typoscript/page.ts
+++ fileadmin/resources/typoscript/page.ts
@@ -3,6 +3,13 @@ page {
         includeCSS {
           file1 = fileadmin\resources\css\main.css
         }
+        headerData {
+            10 = TEXT
+            10.value = <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=yes">
+            #dont work as it should
+            20 = TEXT
+            20.value = <title>JozeeDesign</title>
+        }
         10 = FLUIDTEMPLATE
         10 {
           file.stdWrap.cObject = CASE
@@ -10,52 +17,7 @@ page {
             key.data = levelfield:-1, backend_layout_next_level, slide
             key.override.field = backend_layout
             default = TEXT
-            default.value = fileadmin\resources\templates\main.html
-          }
-          variables {
-              content < styles.content.get
-              content.select.where = colPos=10
-              MAINMENU = HMENU
-              MAINMENU {
-                1 = TMENU
-                1.wrap = <ul class="nav-list-wrapper">|</ul>
-                1.NO {
-                  doNotLinkIt = 1
-                  wrapItemAndSub (
-                    <a href="/index.php?id=1" class="first">
-                        <img src="fileadmin\resources\images\logo.jpg" title="JozeeDesign" alt="Logo" class="logo">
-                    </a>
-                    <div class="center">
-                    <li>|</li> |*| <li>|</li> |*| </div> <li class="last">|</li>
-                  )
-                  stdWrap.cObject = CASE
-                  stdWrap.cObject {
-                    key.field = doktype
-                    default = TEXT
-                    default {
-                      typolink.parameter.field = uid
-                      field = title
-                      stdWrap.htmlSpecialChars = 1
-                    }
-                    # pagetype shortcut
-                    4 = TEXT
-                    4 {
-                      field = title
-                      typolink.parameter.field = shortcut
-                    }
-                    # page typo3 external url
-                    3 = TEXT
-                    3 {
-                      field = title
-                      typolink.parameter.field = url
-                      typolink.extTarget.field = target
-                    }
-                  }
-                }
-                1.ACT = 1
-                1.ACT {
-                  wrapItemAndSub = <li class="active">|</li>
-                }}
+            default.value = fileadmin\resources\templates\home.html
           }
         }
 }
---
+++ fileadmin/resources/typoscript/variables/variables.ts
@@ -0,0 +1,84 @@
+page.10.variables {
+    MAINMENU = HMENU
+    MAINMENU {
+      1 = TMENU
+      1.wrap = <ul class="nav-list-wrapper">|</ul>
+      1.NO {
+        doNotLinkIt = 1
+        wrapItemAndSub (
+          <a href="/index.php?id=1" class="first">
+              <img src="fileadmin\resources\images\logo.jpg" title="JozeeDesign" alt="Logo" class="logo">
+          </a>
+          <div class="center">
+          <li>|</li> |*| <li>|</li> |*| </div> <li class="last">|</li>
+        )
+        stdWrap.cObject = CASE
+        stdWrap.cObject {
+          key.field = doktype
+          default = TEXT
+          default {
+            typolink.parameter.field = uid
+            field = title
+            stdWrap.htmlSpecialChars = 1
+          }
+          # pagetype shortcut
+          4 = TEXT
+          4 {
+            field = title
+            typolink.parameter.field = shortcut
+          }
+          # page typo3 external url
+          3 = TEXT
+          3 {
+            field = title
+            typolink.parameter.field = url
+            typolink.extTarget.field = target
+          }
+        }
+      }
+      1.ACT = 1
+      1.ACT {
+        wrapItemAndSub = <li class="active">|</li>
+      }
+    }
+    MOBILEMENU = HMENU
+    MOBILEMENU {
+      1 = TMENU
+      1.wrap = <ul class="list-wrapper"><div class="flex-wrapper">|</div></ul>
+      1.NO {
+        doNotLinkIt = 1
+        wrapItemAndSub (
+          <li class="first">|</li> |*| <li>|</li> |*| <li class="last">|</li>
+        )
+        stdWrap.cObject = CASE
+        stdWrap.cObject {
+          key.field = doktype
+          default = TEXT
+          default {
+            typolink.parameter.field = uid
+            field = title
+            stdWrap.htmlSpecialChars = 1
+          }
+          # pagetype shortcut
+          4 = TEXT
+          4 {
+            field = title
+            typolink.parameter.field = shortcut
+          }
+          # page typo3 external url
+          3 = TEXT
+          3 {
+            field = title
+            typolink.parameter.field = url
+            typolink.extTarget.field = target
+          }
+        }
+      }
+      1.ACT = 1
+      1.ACT {
+        wrapItemAndSub = <li class="active">|</li>
+      }
+    }  
+    HEADERSLIDER < styles.content.get
+    HEADERSLIDER.select.where = colPos=10
+}
