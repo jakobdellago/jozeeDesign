1c10798409849b0f39aedd40902af28f785c1492
Jakob <jakobdellago@gmail.com>
2018-10-06 20:37:21 +0200

    task (global):
    - add favicons
    - add cooluri
    - other small stuff

--- fileadmin/resources/typoscript/page.ts
+++ fileadmin/resources/typoscript/page.ts
@@ -1,16 +1,35 @@
 page = PAGE
 page {
+        typeNum = 0
         config.contentObjectExceptionHandler = 0
         
         includeCSS {
-          file1 = fileadmin\resources\css\main.css
+          file1 = fileadmin/resources/css/main.css
         }
         headerData {
             10 = TEXT
             10.value = <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=yes">
-            #dont work as it should
+            
+            #Favicons BEGIN
             20 = TEXT
-            20.value = <title>JozeeDesign</title>
+            20.value = <link rel="apple-touch-icon" sizes="180x180" href="/fileadmin/resources/images/favicons/apple-touch-icon.png">
+            30 = TEXT
+            30.value = <link rel="icon" type="image/png" sizes="32x32" href="/fileadmin/resources/images/favicons/favicon-32x32.png">
+            40 = TEXT
+            40.value = <link rel="icon" type="image/png" sizes="16x16" href="/fileadmin/resources/images/favicons/favicon-16x16.png">
+            50 = TEXT
+            50.value = <link rel="manifest" href="/fileadmin/resources/images/favicons/site.webmanifest">
+            60 = TEXT
+            60.value = <link rel="mask-icon" href="/fileadmin/resources/images/favicons/safari-pinned-tab.svg" color="#5bbad5">
+            70 = TEXT
+            70.value = <link rel="shortcut icon" href="/fileadmin/resources/images/favicons/favicon.ico">
+            80 = TEXT
+            80.value = <meta name="msapplication-TileColor" content="#da532c">
+            90 = TEXT
+            90.value = <meta name="msapplication-config" content="/fileadmin/resources/images/favicons/browserconfig.xml">
+            100 = TEXT
+            100.value = <meta name="theme-color" content="#ffffff">
+            #Favicons END
         }
         10 = FLUIDTEMPLATE
         10 {
@@ -19,7 +38,7 @@ page {
                 key.data = levelfield:-1, backend_layout_next_level, slide
                 key.override.field = backend_layout
                 default = TEXT
-                default.value = fileadmin\resources\templates\home.html
+                default.value = fileadmin/resources/templates/home.html
             }
 
             partialRootPaths {
--- fileadmin/resources/typoscript/setup.ts
+++ fileadmin/resources/typoscript/setup.ts
@@ -3,3 +3,6 @@
 <INCLUDE_TYPOSCRIPT: source="FILE:fileadmin/resources/typoscript/libs.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:fileadmin/resources/typoscript/mask/fluidtemplates.ts">
 
+config.baseURL = http://www.jozeedesign.at/
+config.tx_cooluri_enable = 0
+config.redirectOldLinksToNew = 0
