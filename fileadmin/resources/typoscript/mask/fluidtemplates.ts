2e892d3c5787f533c2ee1dd5640db22a917dd4f2
Jakob <jakobdellago@gmail.com>
2018-10-15 12:18:38 +0200

    task (typoscript):
    - remove unnecessary fluidtemplate dataprocessors
    - add sorting to product_link (sort by createDate DESC)

--- fileadmin/resources/typoscript/mask/fluidtemplates.ts
+++ fileadmin/resources/typoscript/mask/fluidtemplates.ts
@@ -1,79 +1,4 @@
 tt_content {
-	# Slideshow
-	mask_product_list = FLUIDTEMPLATE
-	mask_product_list {
-		
-		file = fileadmin\resources\templates\mask\content\product_list.html
-
-		partialRootPaths.10 = fileadmin/resources/templates/mask/content/Partials/
- 
-	    dataProcessing.10 = TYPO3\CMS\Frontend\DataProcessing\DatabaseQueryProcessor
-	    dataProcessing.10 {
-	        # regular if syntax
-	        # if.isTrue.field = records
-
-	        # the table name from which the data is fetched from
-	        # + stdWrap
-	        table = tt_content
-
-	        # All properties from .select can be used directly
-	        # + stdWrap
-	        colPos = 0
-	        pidInList = 8
-	        recursive = 1
-
-	        # The target variable to be handed to the ContentObject again, can
-	        # be used in Fluid e.g. to iterate over the objects. defaults to
-	        # "records" when not defined
-	        # + stdWrap
-	        as = products
-
-	        # The fetched records can also be processed by DataProcessors.
-	        # All configured processors are applied to every row of the result.
-	        dataProcessing {
-	            10 = TYPO3\CMS\Frontend\DataProcessing\FilesProcessor
-	            10 {
-	                references.fieldName = image
-	            }
-	        }
-		}
-	}
-
-	mask_product_list_small = FLUIDTEMPLATE
-	mask_product_list_small {
-	
-		file = fileadmin\resources\templates\mask\content\product_list_small.html
-
-	    dataProcessing.10 = TYPO3\CMS\Frontend\DataProcessing\DatabaseQueryProcessor
-	    dataProcessing.10 {
-	        # regular if syntax
-	        # if.isTrue.field = records
-
-	        # the table name from which the data is fetched from
-	        # + stdWrap
-	        table = tt_content
-
-	        # All properties from .select can be used directly
-	        # + stdWrap
-	        colPos = 0
-	        pidInList = 8
-
-	        # The target variable to be handed to the ContentObject again, can
-	        # be used in Fluid e.g. to iterate over the objects. defaults to
-	        # "records" when not defined
-	        # + stdWrap
-	        as = products
-
-	        # The fetched records can also be processed by DataProcessors.
-	        # All configured processors are applied to every row of the result.
-	        dataProcessing {
-	            10 = TYPO3\CMS\Frontend\DataProcessing\FilesProcessor
-	            10 {
-	                references.fieldName = image
-	            }
-	        }
-		}
-	}
 
 	mask_product_link = FLUIDTEMPLATE
 	mask_product_link {
@@ -96,6 +21,7 @@ tt_content {
 	        colPos = 0
 	        pidInList = 8
 	        recursive = 1
+	        orderBy = crdate DESC
 
 	        # The target variable to be handed to the ContentObject again, can
 	        # be used in Fluid e.g. to iterate over the objects. defaults to
